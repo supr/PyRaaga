87f961d50d0deb0a2a158b81ae86dd6e912cc9ed
Prudhvi Krishna Surapaneni <me@prudhvi.net>
2011-06-09 12:30:57 -0500

    Add Track Metadata to support Music director, Artists(Singers) and Lyricists.

--- metadata/Track.py
+++ metadata/Track.py
@@ -1,6 +1,9 @@
 class Track(object):
-    def __init__(self, title):
+    def __init__(self, title, artists=[], lyricist=None, music=None):
         self.title = title
+        self.artists = artists
+        self.lyricist = lyricist
+        self.music = music
 
     def set_artists(self, artists=[]):
         self.artists = artists
@@ -8,9 +11,26 @@ class Track(object):
     def set_lyricist(self, lyricist=None):
         self.lyricist = lyricist
 
+    def set_music(self, music=None):
+        self.music = music
+
     def __dict__(self):
         return dict(
             Title = self.title,
             Artists = self.artists,
-            Lyricist = self.lyricist
+            Lyricist = self.lyricist,
+            Music = self.music,
             )
+
+    def __str__(self):
+        return self.title
+
+    def __unicode__(self):
+        return self.title
+
+    def __eq__(self, other):
+        if(isinstance(other, Track)):
+            return ((self.title == other.title) and (self.artists == other.artists) 
+                    and (self.lyricist == other.lyricist) and (self.music == other.music))
+        else:
+            return False
