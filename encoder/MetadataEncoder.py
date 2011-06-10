784bceeb3d27aa1e57e335fab3520e1afa41168b
Prudhvi Krishna Surapaneni <me@prudhvi.net>
2011-06-10 14:01:41 -0500

    Handle JSONEncoding with default handler for non Metadata types

--- encoder/MetadataEncoder.py
+++ encoder/MetadataEncoder.py
@@ -6,3 +6,4 @@ class MetadataJSONEncoder(JSONEncoder):
     def default(self, obj):
         if isinstance(obj, Movie) or isinstance(obj, Track):
             return obj.__dict__
+        return JSONEncoder.default(self, obj)
