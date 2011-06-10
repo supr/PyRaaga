c7e8c30984e05f87712178caf7ce16b7b4ece47d
Prudhvi Krishna Surapaneni <me@prudhvi.net>
2011-06-10 13:58:17 -0500

    Add JSONEncoder for Metadata Objects and TestCases

---
+++ encoder/MetadataEncoder.py
@@ -0,0 +1,8 @@
+from json import JSONEncoder
+from metadata.Movie import Movie
+from metadata.Track import Track
+
+class MetadataJSONEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Movie) or isinstance(obj, Track):
+            return obj.__dict__
--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 import unittest
+from json import dumps
 from raaga import Raaga
 from metadata.Track import Track
 from metadata.Movie import Movie
+from encoder.MetadataEncoder import MetadataJSONEncoder
 
 class RaagaTestCase(unittest.TestCase):
     def setUp(self):
@@ -20,5 +22,23 @@ class RaagaTestCase(unittest.TestCase):
         m = Movie("Madhura Geetham", [t1,t2])
         self.assertEqual(self.raaga.get_movie_info('A0002038'), m)
         
+class MetadataJSONEncoderTestCase(unittest.TestCase):
+    def setUp(self):
+        self.raaga = Raaga()
+
+    def test_default(self):
+        t1 = Track("Anandha Ragam")
+        t1.set_artists(['Susheela'])
+        t1.set_music(['Illayaraja'])
+        t2 = Track("Navvulalona")
+        t2.set_artists(['SP. Balasubramaniam','Janaki'])
+        t2.set_music(['Illayaraja'])
+        m = Movie("Madhura Geetham", [t1,t2])
+        jstr = '{"tracks": [{"lyricist": null, "artists": ["Susheela"], "music": ["Illayaraja"], "title": "Anandha Ragam"},\
+ {"lyricist": null, "artists": ["SP. Balasubramaniam", "Janaki"],\
+ "music": ["Illayaraja"], "title": "Navvulalona"}], "name": "Madhura Geetham"}'
+        mjs = dumps(m, cls = MetadataJSONEncoder)
+        self.assertEqual(mjs, jstr)
+
 if __name__ == "__main__":
     unittest.main()
