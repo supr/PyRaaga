c793d4dfb20c9eeb17a3c41bcbbba6acf9b8d115
Prudhvi Krishna Surapaneni <me@prudhvi.net>
2011-06-10 14:18:17 -0500

    Refactor TestCases

--- test.py
+++ test.py
@@ -5,39 +5,30 @@ from metadata.Track import Track
 from metadata.Movie import Movie
 from encoder.MetadataEncoder import MetadataJSONEncoder
 
-class RaagaTestCase(unittest.TestCase):
+class BaseTestCase(unittest.TestCase):
     def setUp(self):
         self.raaga = Raaga()
-        
+        track1 = Track("Anandha Ragam")
+        track1.set_artists(['Susheela'])
+        track1.set_music(['Illayaraja'])
+        track2 = Track("Navvulalona")
+        track2.set_artists(['SP. Balasubramaniam', 'Janaki'])
+        track2.set_music(['Illayaraja'])
+        self.movie = Movie("Madhura Geetham", [track1, track2])
+
+class RaagaTestCase(BaseTestCase):
     def testGetMovieIds(self):
         self.assertNotEqual(self.raaga.get_movie_ids(), [])
 
     def testGetMovieInfo(self):
-        t1 = Track("Anandha Ragam")
-        t1.set_artists(['Susheela'])
-        t1.set_music(['Illayaraja'])
-        t2 = Track("Navvulalona")
-        t2.set_artists(['SP. Balasubramaniam','Janaki'])
-        t2.set_music(['Illayaraja'])
-        m = Movie("Madhura Geetham", [t1,t2])
-        self.assertEqual(self.raaga.get_movie_info('A0002038'), m)
+        self.assertEqual(self.raaga.get_movie_info('A0002038'), self.movie)
         
-class MetadataJSONEncoderTestCase(unittest.TestCase):
-    def setUp(self):
-        self.raaga = Raaga()
-
+class MetadataJSONEncoderTestCase(BaseTestCase):
     def test_default(self):
-        t1 = Track("Anandha Ragam")
-        t1.set_artists(['Susheela'])
-        t1.set_music(['Illayaraja'])
-        t2 = Track("Navvulalona")
-        t2.set_artists(['SP. Balasubramaniam','Janaki'])
-        t2.set_music(['Illayaraja'])
-        m = Movie("Madhura Geetham", [t1,t2])
         jstr = '{"tracks": [{"lyricist": null, "artists": ["Susheela"], "music": ["Illayaraja"], "title": "Anandha Ragam"},\
  {"lyricist": null, "artists": ["SP. Balasubramaniam", "Janaki"],\
  "music": ["Illayaraja"], "title": "Navvulalona"}], "name": "Madhura Geetham"}'
-        mjs = dumps(m, cls = MetadataJSONEncoder)
+        mjs = dumps(self.movie, cls = MetadataJSONEncoder)
         self.assertEqual(mjs, jstr)
 
 if __name__ == "__main__":
